e11fbbd4a25cbde6169772fda7e056e1fbca737d
zeopean <1412512785@qq.com>
2015-12-02 22:51:10 +0800

    测试git

--- mobile.php
+++ mobile.php
@@ -29,14 +29,6 @@ function getLocationByMobile($mobile="18665746639")
 	    return $result['reason']."(".$result['error_code'].")";
 	}
 
-	/*
-    "province":"浙江",
-    "city":"杭州",
-    "areacode":"0571",
-    "zip":"310000",
-    "company":"中国移动",
-    "card":"移动动感地带卡"
-    */
 }
 
 var_dump(getLocationByMobile());
